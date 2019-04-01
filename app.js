5fe56b097011f7d9e7ccfacd8308fb4b08e8cb15
root <root@domaine.tld>
2019-04-01 12:22:14 +0200

    TP4-ESIR-STEP3

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 
 const usersRouter = require('./routes/users-v1')
 const usersModel = require('./model/users')
-
+const helmet=require('helmet')
 const app = express()
 
 app.use(bodyParser.json())
-
+app.use(helmet.noSniff())
+app.use(helmet.xssFilter())
 // On injecte le model dans le router. Ceci permet de supprimer la dépendance
 // directe entre le router et le modele
 app.use('/v1/users', usersRouter(usersModel))
